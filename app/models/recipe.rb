f20198b800bdea856c32fc6157db7c0056a65951
Vanessa Toro <vanetoro84@gmail.com>
2018-07-09 20:02:49 +0000

    automatically backed up by learn

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,6 +2,7 @@ class Recipe < ActiveRecord::Base
   has_many :ingredients
 
   def ingredients_attributes=(ingredients_attributes)
+    binding.pry
     ingredients_attributes.each do  |ingredient_attribute|
       self.ingredients = Ingredient.build(ingredient_attribute)
     end
