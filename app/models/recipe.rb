73a63f024b4feaa23cbf910481931d8e2a73746e
Vanessa Toro <vanetoro84@gmail.com>
2018-07-09 17:33:47 -0400

    Done.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,7 +2,6 @@ class Recipe < ActiveRecord::Base
   has_many :ingredients
 
   def ingredients_attributes=(ingredients_attributes)
-
     ingredients_attributes.each do  |ingredient_attribute|
       if !ingredient_attribute[:name].blank?
         self.ingredients << Ingredient.new(ingredient_attribute)
